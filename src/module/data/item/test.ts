a3abe40c152cb4e0d8204d75a54411dc7eb69b42
Luke Abby <luke@lukeabby.dev>
2025-03-14 16:36:57 -0700

    Fix the implementation of SystemDataModel.mixed

--- src/module/data/item/test.ts
+++ src/module/data/item/test.ts
@@ -2,7 +2,23 @@ import { SystemDataModel } from "@data/abstract.ts"
 import { ItemDataModel } from "./base.ts"
 import { BasicInformationTemplate } from "./templates/basic-information.ts"
 
-class TestSystemData extends SystemDataModel<TestSystemSchema> {}
+declare class TestSystemData extends SystemDataModel<TestSystemSchema> {
+	constructor(...args: any[])
+
+	static name: "foo"
+
+	static static1: 123
+	instance1: "foo"
+}
+
+declare class TestSystemData2 extends SystemDataModel<TestSystemSchema2> {
+	constructor(...args: any[])
+
+	static name: "bar"
+
+	protected static static2: 456
+	instance2: "bar"
+}
 
 export class TestItemData1 extends foundry.abstract.TypeDataModel<TestItemSchema, Item> {}
 
@@ -10,13 +26,31 @@ export class TestItemData2 extends SystemDataModel<TestItemSchema, Item> {}
 
 export class TestItemData3 extends ItemDataModel<TestItemSchema> {}
 
-export class TestItemData4 extends SystemDataModel.mixin<[typeof TestSystemData], TestItemSchema>(TestSystemData) {}
+export class TestItemData4 extends SystemDataModel.mixin(TestSystemData, TestSystemData2) {}
+TestItemData4.name
+//            ^ string
+// The given class names are intentionally completely ignored as they're immiscible
 
-export class TestItemData5 extends ItemDataModel.mixin<[typeof BasicInformationTemplate], TestItemSchema>(
-	BasicInformationTemplate,
-) {}
+TestItemData4.static1
+TestItemData4["static2"]
+
+const test = new TestItemData4()
+test.instance1
+test.instance2
+test.schemaProp1
+test.schemaProp2
+
+export class TestItemData5 extends ItemDataModel.mixin(BasicInformationTemplate) {}
 
 type TestItemSchema = {}
-type TestSystemSchema = {}
+
+import fields = foundry.data.fields
+type TestSystemSchema = {
+	schemaProp1: fields.NumberField
+}
+
+type TestSystemSchema2 = {
+	schemaProp2: fields.StringField
+}
 
 // export { TestItemData, type TestItemSchema }
