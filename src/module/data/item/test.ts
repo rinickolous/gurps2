5552a4f4510c4a025fdaa3de092ccb2573ae80d7
Mikolaj Tomczynski <mikolajtomczynski@gmail.com>
2025-03-14 20:56:22 +0000

    add more test cases for error

--- src/module/data/item/test.ts
+++ src/module/data/item/test.ts
@@ -8,14 +8,15 @@ export class TestItemData2 extends SystemDataModel<TestItemSchema, Item> {}
 
 export class TestItemData3 extends ItemDataModel<TestItemSchema> {}
 
-export class TestItemData4 extends SystemDataModel.mixin<[typeof BasicInformationTemplate], TestItemSchema>(
-	BasicInformationTemplate,
-) {}
+export class TestItemData4 extends SystemDataModel.mixin<[typeof TestSystemData], TestItemSchema>(TestSystemData) {}
 
 export class TestItemData5 extends ItemDataModel.mixin<[typeof BasicInformationTemplate], TestItemSchema>(
 	BasicInformationTemplate,
 ) {}
 
+class TestSystemData extends SystemDataModel<TestSystemSchema> {}
+
 type TestItemSchema = {}
+type TestSystemSchema = {}
 
 // export { TestItemData, type TestItemSchema }
